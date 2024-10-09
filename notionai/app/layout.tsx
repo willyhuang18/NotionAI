0622b6f9db1b708591cb5bf40a32f4dbffed9a26
willyhuang18 <willyhuang18@gmail.com>
2024-10-09 16:21:43 -0700

    building header

--- notionai/app/layout.tsx
+++ notionai/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/Header";
 
 
 export const metadata: Metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({
     <ClerkProvider>
     <html lang="en">
       <body>
+        <Header />
         {children}
       </body>
     </html>
