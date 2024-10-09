05e5d5e50e4915496db72be6b128039e051ea617
willyhuang18 <willyhuang18@gmail.com>
2024-10-09 16:44:18 -0700

    adding the sidebar ui

--- notionai/components/Header.tsx
+++ notionai/components/Header.tsx
@@ -1,6 +1,26 @@
+"use client"
+import { SignedIn, SignedOut, SignInButton, UserButton, useUser } from "@clerk/nextjs"
+
 function Header() {
+    const {user} = useUser();
   return (
-    <div>Header</div>
+    <div className="flex items-center justify-between p-5">
+        {user && (
+            <h1 className="text-2xl">
+            {user?.firstName} 
+            {`s`} space
+            </h1>
+        )}
+        {/* Breadcrumbs */}
+        <div>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
+            <SignedOut>
+                <SignInButton />
+            </SignedOut>
+        </div>
+    </div>
   )
 }
 
---
+++ notionai/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+function Sidebar() {
+  return (
+    <div >
+
+    </div>
+  )
+}
+
+export default Sidebar
