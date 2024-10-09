eb631f4d9637df4b0e9635872fba554bf9c8a27d
willyhuang18 <willyhuang18@gmail.com>
2024-10-09 16:56:05 -0700

    adding the popup sidebar

---
+++ notionai/components/NewDocumentButton.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./ui/button"
+
+function NewDocumentButton() {
+  return (
+    <div>
+        <Button>
+        NewDocumentButton
+        </Button>
+        </div>
+  )
+}
+
+export default NewDocumentButton
--- notionai/components/Sidebar.tsx
+++ notionai/components/Sidebar.tsx
@@ -1,9 +1,37 @@
 "use client"
+import { MenuIcon } from "lucide-react"
+import NewDocumentButton from "./NewDocumentButton"
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+  
 
 function Sidebar() {
   return (
-    <div >
+    <div className="p-2 md:p-5 bg-gray-200 relative">
+        <Sheet>
+            <SheetTrigger>
+                <MenuIcon className="p-2 hover:opacity-30 rounded-lg" size={40}/>
+            </SheetTrigger>
+            <SheetContent side="left">
+                <SheetHeader>
+                <SheetTitle>Menu</SheetTitle>
+                <div>
+                    {/* option */}
+                </div>
+                
+                </SheetHeader>
+            </SheetContent>
+        </Sheet>
+        <div className="hidden md:inline">
+            <NewDocumentButton />
 
+        </div>
     </div>
   )
 }
