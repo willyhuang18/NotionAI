81d73547f1c14dc1a5d4a6f8bf5ff9e560786d1f
willyhuang18 <willyhuang18@gmail.com>
2024-10-27 23:18:09 -0700

    adding update title feature

--- notionai/components/Document.tsx
+++ notionai/components/Document.tsx
@@ -1,10 +1,53 @@
-'use client'
-import React from 'react'
+"use client";
+import React, { FormEvent, useEffect, useState, useTransition } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "@/firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
 
-function Document({id} :{id}) {
+function Document({ id }: { id: string }) {
+  const [data, loading, error] = useDocumentData(doc(db, "documents", id));
+  const [input, setInput] = useState("");
+  const [isUpdating, startTransition] = useTransition();
+
+    useEffect(() => {
+        if (data) {
+          setInput(data.title);
+      }
+  }, [data]);
+  const updateTitle = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (input.trim()) {
+      startTransition(async () => {
+        await updateDoc(doc(db, "documents", id), {
+          title: input,
+        });
+      });
+    }
+  };
+
   return (
-    <div>Document</div>
-  )
+    <div>
+      <div>
+        <form onSubmit={updateTitle}>
+          {/* update title */}
+          <Input value={input} onChange={(e) => setInput(e.target.value)} />
+          <Button disabled={isUpdating} type="submit">
+            {isUpdating ? "Updating..." : "Update"}
+          </Button>
+          {/* if statement */}
+          {/* isOwner && inviter, */}
+        </form>
+      </div>
+      <div>
+        {/* ManageUsers */}
+        {/* Avatars */}
+      </div>
+      {/* collaborative Editor */}
+    </div>
+  );
 }
 
-export default Document
+export default Document;
