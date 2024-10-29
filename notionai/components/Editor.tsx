86261e5fdfaf97a047b38d9aa257653873a236bc
willyhuang18 <willyhuang18@gmail.com>
2024-10-29 00:10:05 -0700

    setting up the DarkMode of the document

--- notionai/components/Editor.tsx
+++ notionai/components/Editor.tsx
@@ -1,17 +1,50 @@
-import React from 'react'
+import { useRoom } from "@liveblocks/react/suspense";
+import React, { useEffect, useState } from "react";
+import * as Y from "yjs";
+import { LiveblocksYjsProvider } from "@liveblocks/yjs";
+import { Button } from "./ui/button";
+import { MoonIcon, SunIcon } from "lucide-react";
 
 function Editor() {
-  return (
-      <div className='max-w-6xl mx-auto'>
-          <div>
-              {/* AI translateDocument */}
-              {/* ChatToDocument AI */}
+  const room = useRoom();
+  const [doc, setDoc] = useState<Y.Doc>();
+  const [provider, setProvider] = useState<LiveblocksYjsProvider>();
+  const [darkMode, setDarkMode] = useState(false);
 
-              {/* Dark mode */}
-          </div>
-          {/* BlockNote */}
+  useEffect(() => {
+    const yDoc = new Y.Doc();
+    const yProvider = new LiveblocksYjsProvider(room, yDoc);
+    setDoc(yDoc);
+    setProvider(yProvider);
+    return () => {
+      yDoc?.destroy();
+      yProvider?.destroy();
+    };
+  }, [room]);
+  if (!doc || !provider) {
+    return null;
+  }
+
+  const style = `hover: text-white ${
+    darkMode
+      ? "text-gray-300 bg-gray-700 hover:bg-gray-100 hover:text-gray-700"
+      : "text-gray-700 bg-gray-200 hover:bg-gray-300 hover:text-gray-700"
+  }`;
+
+  return (
+    <div className="max-w-6xl mx-auto">
+      <div className="flex items-center gap-2 justify-end mb-10">
+        {/* AI translateDocument */}
+        {/* ChatToDocument AI */}
+
+        {/* Dark mode */}
+        <Button className={style} onClick={() => setDarkMode(!darkMode)}>
+          {darkMode ? <SunIcon /> : <MoonIcon />}
+        </Button>
+      </div>
+      {/* BlockNote */}
     </div>
-  )
+  );
 }
 
-export default Editor
+export default Editor;
